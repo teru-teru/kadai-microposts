b64a11a1778004a819ae5ba8c4923dbb8730edb5
teru.teru <hello.loveit.2000@gmail.com>
2018-06-30 11:33:39 +0000

    favorite destroy correction

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :favorites, foreign_key: "like_id", dependent: :destroy
   
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 255}
